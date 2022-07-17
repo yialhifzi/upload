b4480c973d849153f68e1e7ee6781e67bfa5b2c0
yialhifzi <66778154+yialhifzi@users.noreply.github.com>
2022-07-17 11:56:21 +0300

    Update Hello.java

--- Hello.java
+++ Hello.java
@@ -1,7 +1,7 @@
 public class Hello {
 	public static void main(String[] args) {
 		System.out.println("My name is Yazeed");
-        System.out.println("I am 999 years old");
+        	System.out.println("I am 999 years old");
 		System.out.println("My hometown is This PC");
 	}
 }
